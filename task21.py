2dd9c54cc60e8f3606e5589adac7968d5316c28b
Semetei Abaildaev <semetei.abaildaev@MacBook-Pro-Semetei.local>
2020-01-13 12:30:59 +0600

    P2T21

---
+++ task21.py
@@ -0,0 +1,9 @@
+def summary(number1, number2, number3):
+    result = number1 + number2 + number3
+    return print(f'Сумма: {result}')
+
+num1 = int(input("Введите 1-е число: "))
+num2 = int(input("Введите 2-е число: "))
+num3 = int(input("Введите 3-е число: "))
+
+summary(num1, num2, num3)
